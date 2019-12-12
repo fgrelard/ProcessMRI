a50f9f00b3f58421bafc15d61911ca5fbdfc05f6
Florent Grélard <gm.florent@gmail.com>
2019-12-12 11:55:32 +0100

    Add fill holes to cavity

--- src/segmentation.py
+++ src/segmentation.py
@@ -266,6 +266,7 @@ def detect_cavity(image):
     distance = ndi.distance_transform_edt(image_copy)
     distance =  cv2.blur(distance, (3, 3)) # blur the image
     seed = find_local_maximum_dt(distance, point)
-    seg = sitk.ConfidenceConnected(sitk.GetImageFromArray(distance), seedList=[seed], numberOfIterations=1, multiplier=3, initialNeighborhoodRadius=1, r²eplaceValue=255)
-    return sitk.GetArrayFromImage(seg)
-    return image_copy
+    seg = sitk.ConfidenceConnected(sitk.GetImageFromArray(distance), seedList=[seed], numberOfIterations=1, multiplier=3, initialNeighborhoodRadius=1, replaceValue=255)
+    seg_array = sitk.GetArrayFromImage(seg)
+    seg_array = ndi.morphology.binary_fill_holes(seg_array)
+    return seg_array
